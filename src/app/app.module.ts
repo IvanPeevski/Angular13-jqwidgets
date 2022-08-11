8acfa931a1cd7aabde96980e2d7f8301d526fbd4
IvanPeevski <37153196+IvanPeevski@users.noreply.github.com>
2022-08-11 12:48:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { jqxGridModule } from 'jqwidgets-ng/jqxgrid';
+import { jqxTreeModule } from 'jqwidgets-ng/jqxtree';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    jqxGridModule,
+    jqxTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
